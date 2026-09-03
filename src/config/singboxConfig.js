2d0c0a68c9c8adda172ca3b1639abcb8a8f5889a
hhzol <kdkhz@outlook.com>
2026-09-03 23:37:35 +0800

    initialization

--- src/config/singboxConfig.js
+++ src/config/singboxConfig.js
@@ -4,60 +4,23 @@
  */
 
 export const SING_BOX_CONFIG = {
-	dns: {
-		servers: [
-			{
-				type: "tcp",
-				tag: "dns_proxy",
-				server: "1.1.1.1",
-				detour: "🚀 节点选择",
-				domain_resolver: "dns_resolver"
-			},
-			{
-				type: "https",
-				tag: "dns_direct",
-				server: "dns.alidns.com",
-				domain_resolver: "dns_resolver"
-			},
-			{
-				type: "udp",
-				tag: "dns_resolver",
-				server: "223.5.5.5"
-			},
-			{
-				type: "fakeip",
-				tag: "dns_fakeip",
-				inet4_range: "198.18.0.0/15",
-				inet6_range: "fc00::/18"
-			}
-		],
-		rules: [
-			{
-				rule_set: "geolocation-!cn",
-				query_type: [
-					"A",
-					"AAAA"
-				],
-				server: "dns_fakeip"
-			},
-			{
-				rule_set: "geolocation-!cn",
-				query_type: "CNAME",
-				server: "dns_proxy"
-			},
-			{
-				query_type: [
-					"A",
-					"AAAA",
-					"CNAME"
-				],
-				invert: true,
-				action: "predefined",
-				rcode: "REFUSED"
-			}
-		],
-		final: "dns_direct"
-	},
+    dns: {
+        servers: [
+            {"tag": "local", "type": "quic", "server": "223.5.5.5", "server_port": 853},
+            {"tag": "node_dns", "type": "tls", "server": "dns.opendns.com", "domain_resolver": "local"},
+            {"tag": "foreign", "type": "https", "server": "dns.google", "path": "/dns-query","domain_resolver": "local", "detour": "🐟 漏网之鱼"},  
+            {"tag": "fakeip", "type": "fakeip", "inet4_range": "198.18.0.0/15", "inet6_range": "fc00::/18"}
+        ],
+        rules: [
+            {"clash_mode": "direct", "server": "local"},
+            {"clash_mode": "global", "server": "fakeip"},
+            {"rule_set": ["category-ads-all"], "action": "predefined", "rcode": "NOERROR"},
+            {"rule_set": ["geolocation-cn", "cn"], "server": "local"},
+            {"query_type": ["A", "AAAA"], "server": "fakeip", "rewrite_ttl": 1}
+        ],
+        final: "foreign",
+        client_subnet: "0.0.0.0/0"
+    },
 	ntp: {
 		enabled: true,
 		server: 'time.apple.com',
@@ -65,14 +28,14 @@ export const SING_BOX_CONFIG = {
 		interval: '30m'
 	},
 	inbounds: [
-		{ type: 'mixed', tag: 'mixed-in', listen: '0.0.0.0', listen_port: 2080 },
-		{ type: 'tun', tag: 'tun-in', address: '172.19.0.1/30', auto_route: true, strict_route: true, stack: 'mixed' }
+		{"type": "mixed",  "tag": "mixed-in", "listen": "::", "listen_port": 5330, "sniff": true, "set_system_proxy": false},
+    	{"type": "tun", "tag": "tun-in", "interface_name": "momo", "address": ["172.19.0.1/30", "fdfe:dcba:9876::1/126"], "mtu": 9000, "auto_route": false, "strict_route": true, "endpoint_independent_nat": true, "stack": "gvisor", "sniff": true}
+
 	],
 	outbounds: [
 		{ type: "direct", tag: 'DIRECT' }
 	],
 	route: {
-		default_domain_resolver: "dns_resolver",
 		"rule_set": [
 			{
 				"tag": "geosite-geolocation-!cn",
@@ -81,7 +44,10 @@ export const SING_BOX_CONFIG = {
 				"path": "geosite-geolocation-!cn.srs"
 			}
 		],
-		rules: []
+		rules: [],
+		auto_detect_interface: false,
+		default_domain_resolver: "local",
+		final: "🐟 漏网之鱼"
 	},
 	experimental: {
 		cache_file: {
